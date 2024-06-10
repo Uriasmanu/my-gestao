fd117508eb68434d37bf5f6521f5a94351622123
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-06-09 23:15:11 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import AdicionarProduto from './Componentes/AdicionarProduto';
+import AtualizarProduto from './Componentes/AtualizarProduto';
+import MostrarProdutos from './Componentes/MostrarProdutos';
+import RemoverProduto from './Componentes/RemoverProduto';
 
 
 function App() {
   return (
     <div className="App">
+      <MostrarProdutos/>
       <AdicionarProduto/>
+      <RemoverProduto/>
+      <AtualizarProduto/>
     </div>
   );
 }
